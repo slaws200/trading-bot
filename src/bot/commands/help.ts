3150a5f2241b9ebf15efdbf6a4715765f88630bf
v.alafev <v.alafev@dextechnology.com>
2025-09-26 16:35:48 +0300

    microfix

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -6,7 +6,7 @@ export function helpCommand(ctx: Context) {
 /start - запустить бота
 /help - помощь
 /ping - проверить отклик
-/getfundingrates - test
+/getfundingrates - топ-10 токенов по самой высокой волатильности
 /open_interest - открытый интерес указанной валютной пары
 /longshort - Соотношение лонг/шорт позициий указанной валютной пары
 /funding_history - история фандинга указанной валютной пары
